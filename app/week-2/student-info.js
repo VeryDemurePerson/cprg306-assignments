c4589995a9689d27f5567312da9c69469cdde036
VeryDemurePerson <itsyoboitonylai@gmail.com>
2025-05-13 10:22:29 -0600

    Week 2 assignment completed

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() 
+{
+    return (
+        <main>
+            <p>Student Name: LAI NHAT DUONG PHAN</p>
+            <Link href="https://github.com/VeryDemurePerson/cprg306-assignments">My Github</Link>
+            
+        </main>
+    );
+}
