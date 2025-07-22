14b8d2eb8c7a679a1d63323e44984c53da07ff41
Tony <itsyoboitonylai@gmail.com>
2025-07-21 22:02:38 -0600

    week 9 updated

--- app/week-9/_utils/auth-context.js
+++ app/week-9/_utils/auth-context.js
@@ -1,5 +1,5 @@
 "use client";
- 
+
 import { useContext, createContext, useState, useEffect } from "react";
 import {
   signInWithPopup,
@@ -8,35 +8,38 @@ import {
   GithubAuthProvider,
 } from "firebase/auth";
 import { auth } from "./firebase";
- 
+
 const AuthContext = createContext();
- 
+
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
- 
+  const [loading, setLoading] = useState(true);
+
   const gitHubSignIn = () => {
     const provider = new GithubAuthProvider();
     return signInWithPopup(auth, provider);
   };
- 
+
   const firebaseSignOut = () => {
     return signOut(auth);
   };
- 
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false); 
     });
+
     return () => unsubscribe();
-  }, [user]);
- 
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user, gitHubSignIn, firebaseSignOut }}>
+    <AuthContext.Provider value={{ user, loading, gitHubSignIn, firebaseSignOut }}>
       {children}
     </AuthContext.Provider>
   );
 };
- 
+
 export const useUserAuth = () => {
   return useContext(AuthContext);
 };
